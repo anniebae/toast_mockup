e34040520b0012f76c7b702418a3f606cf8daff7
anniebae <bae.annie@gmail.com>
2015-04-08 16:43:13 -0400

    rwd implemented with bootstrap

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-// $(function() {
-//   $("#left-panel").animate({
-//     opacity: 40%;
-//     width: 60%;
-//     height: 576px;
-//     float: left;
-//     }, 1000);
-// });
-
-
 function oneSec() {
-
   setTimeout(function() {
     $('#left-panel').show('slide', {direction: 'left'}, 1000);
     $('#right-panel').show('slide', {direction: 'right'}, 1000),
@@ -17,25 +6,13 @@ function oneSec() {
         $('.logo').show('slide', {direction: 'left'}, 1000);
       }, 1300);     
   }, 1000);
-
 }
 
-
 $(function() {
-  
-oneSec();
-
+  oneSec();
   $('.close').on('click', function() {
       $('#left-panel').hide('slide', {direction: 'left'}, 1000);
       $('#right-panel').hide('slide', {direction: 'right'}, 1000);
       $('.logo').hide('slide', {direction: 'left'}, 1300);
     });  
-
 });
-
-
-    
-
-
-
-
