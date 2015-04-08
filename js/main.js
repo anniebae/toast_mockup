d9a2eefc6283cdc577bc7d1e94c83d664811e0ea
anniebae <bae.annie@gmail.com>
2015-04-08 14:57:36 -0400

    bootstrap required to create rwd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// new WOW (
-//   { offset: 100 }
-// ).init();
-
 // $(function() {
 //   $("#left-panel").animate({
 //     opacity: 40%;
@@ -33,9 +29,7 @@ oneSec();
       $('#left-panel').hide('slide', {direction: 'left'}, 1000);
       $('#right-panel').hide('slide', {direction: 'right'}, 1000);
       $('.logo').hide('slide', {direction: 'left'}, 1300);
-    });
-
-  
+    });  
 
 });
 
